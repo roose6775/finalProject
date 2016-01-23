ebff2cd45153deb6eda3ec9da0d017629cadfd8d
roose6775 <yotie@yandex.ru>
2016-01-23 03:38:39 +0300

    added class Plant for plants in park and PlantDAO

---
+++ src/main/java/project/model/Plant.java
@@ -0,0 +1,24 @@
+package project.model;
+
+/**
+ * Created by Maria on 22.01.16.
+ */
+
+public class Plant {
+    private int id;
+    private String name;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
+
