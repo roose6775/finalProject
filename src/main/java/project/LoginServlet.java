0dab17626daeaa77a5ab04942f9b39f1f75712c3
roose6775 <yotie@yandex.ru>
2016-01-25 01:07:48 +0300

    Changed servlet for login (added searching for account data similar to inserted in data base + informing user about invalid username/password)

--- src/main/java/project/LoginServlet.java
+++ src/main/java/project/LoginServlet.java
@@ -1,5 +1,6 @@
 package project;
 
+import project.DAO.UserDAO;
 import project.model.User;
 
 import javax.servlet.RequestDispatcher;
@@ -8,8 +9,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 @WebServlet(
         value = "/login",
@@ -27,10 +31,11 @@ public class LoginServlet extends HttpServlet{
 
         RequestDispatcher requestDispatcher;
         User loginUser = new User();
+        PrintWriter out;
 
         if (req.getParameter("loginNickname").isEmpty() || req.getParameter("loginPassword").isEmpty()) {
             requestDispatcher = req.getRequestDispatcher("WEB-INF/jsp/login.jsp");
-            PrintWriter out = resp.getWriter();
+            out = resp.getWriter();
             out.println("<p style='color:red;text-align:center'> No field can be empty. Fill all the fields.</p>");
             requestDispatcher.include(req, resp);
         } else {
@@ -38,9 +43,33 @@ public class LoginServlet extends HttpServlet{
             loginUser.setPassword(req.getParameter("loginPassword"));
         }
 
+        System.out.println(loginUser.getNickname() + "!!!!!!" + loginUser.getPassword());
 
+        UserDAO userDAO = new UserDAO();
+        String id = req.getParameter("name");
+        Statement st = null;
+        HttpSession session = req.getSession(true);
+        ResultSet rs;
 
-        System.out.println(loginUser.getNickname());
+        try {
+            st = userDAO.getConnection().createStatement();
+            rs = st.executeQuery("SELECT * FROM users WHERE nickname = '" + loginUser.getNickname() // put in DAO!!
+                    + "' AND password = '" + loginUser.getPassword() + "'");
+            if (rs.next()) {
+                session.setAttribute("id", id);
+                resp.sendRedirect("success.jsp");
+            } else {
+                requestDispatcher = req.getRequestDispatcher("WEB-INF/jsp/login.jsp");
+                out = resp.getWriter();
+                out.println("<p style='color:red;text-align:center'>Invalid login or password. Try again</p>");
+                requestDispatcher.include(req, resp);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //System.out.println(loginUser.getNickname());
     }
 }
 
