14ac7136cc96fdb74df0fc65ca7e7cd4a11f8ea2
roose6775 <yotie@yandex.ru>
2016-01-24 15:29:18 +0300

    added class User for user accounts

---
+++ src/main/java/project/model/User.java
@@ -0,0 +1,42 @@
+package project.model;
+
+public class User {
+
+    private int id;
+    private String name;
+    private String nickname;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String nickname) {
+        this.password = password;
+    }
+
+}
